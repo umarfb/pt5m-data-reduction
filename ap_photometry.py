e545d8748c07fd9ba5b237c70d78dd773956bfb5
umarfb <44232856+umarfb@users.noreply.github.com>
2018-11-09 12:38:31 +0000

    Update ap_photometry.py
    - Calculate instrumental magnitudes and corresponding error

--- ap_photometry.py
+++ ap_photometry.py
@@ -10,8 +10,14 @@ from photutils import aperture_photometry, CircularAperture, CircularAnnulus
 
 path = '/local/php18ufb/backed_up_on_astro3/mini_project01/data20183009/aperture_photometry/'
 
+filter_name = input('Specify filter name: ')
+if filter_name.upper() in ('B', 'V', 'R', 'I'):
+    filter_name = filter_name.upper()
+else:
+    raise ValueError('filter does not exist for pt5m')
+
 # load image data
-fname = 'I_final'
+fname = filter_name + '_final'
 img = fits.getdata(fname + '.fits')
 
 # creates ZScale interval as used by DS9
@@ -22,27 +28,27 @@ interval.get_limits(img)
 # pixels that are above or below a specified sigma level from the median are discarded 
 # and the statistics are recalculated
 mean, median, std = sigma_clipped_stats(img, sigma=3, iters=5)
-print(mean, median, std)
+#print(mean, median, std)
 
 # subtract background and use DAOStarFinder to locate stars in the image that have FWHMs
-# of arounf 3 pixels and have peaks approximately 4-sigma above the background
-daofind = DAOStarFinder(fwhm=3.0, threshold=4.*std)
+# of around 5 pixels and have peaks approximately 4-sigma above the background
+daofind = DAOStarFinder(fwhm=5, threshold=4.*std)
 sources = daofind(img - median)
 for col in sources.colnames:
     sources[col].info.format = '%.8g'   # for consistent table output
-print(sources)                          # gets table of sources and their pixel positions
+#print(sources)                          # gets table of sources and their pixel positions
 positions = (sources['xcentroid'], sources['ycentroid'])    # list of source pixel positions
 
 # create circular aperture and annulus aperture objects, radius in pixel scale
-apertures = CircularAperture(positions, r=4.)
-annulus_apertures = CircularAnnulus(positions, r_in=8., r_out=11.)
+apertures = CircularAperture(positions, r=5)
+annulus_apertures = CircularAnnulus(positions, r_in=10, r_out=20)
 
 # perform photometry in both apertures
 apers = [apertures, annulus_apertures]
 phot_table = aperture_photometry(img, apers)
 for col in phot_table.colnames:
     phot_table[col].info.format = '%.8g'    # for consistent table output
-print(phot_table)
+#print(phot_table)
 
 # to calculate the mean local background within the circular annulus aperture, we need to divide
 # its sum by its area, which can be calculated using the area() method
@@ -54,20 +60,49 @@ bkg_sum = bkg_mean * apertures.area()
 final_sum = phot_table['aperture_sum_0'] - bkg_sum
 phot_table['residual_aperture_sum'] = final_sum
 phot_table['residual_aperture_sum'].info.format = '%.8g'    # for consistent table output
-#print(phot_table['residual_aperture_sum'])
+
+# removes sources with negative counts
+rm_idx = []
+for row in phot_table:
+    if np.sign(row['residual_aperture_sum']) == -1:
+        idx = int(row['id']) - 1
+        rm_idx.append(idx)
+    else:
+        pass
+phot_table.remove_rows(rm_idx)
+
+# calculate uncertainty in counts
+n_pix = apertures.area()
+n_sky = annulus_apertures.area()
+readnoise = 7
+c0 = n_pix * (1 + (n_pix/n_sky))
+
+count_err = np.sqrt(phot_table['aperture_sum_0'] + c0 * (phot_table['aperture_sum_1'] + readnoise ** 2))
+phot_table['count_err'] = count_err
+
+# calculate error in magnitde
+mag_err = (1.0857 / phot_table['aperture_sum_0']) * count_err
+phot_table['mag_inst_err'] = mag_err
+
+# calculate instrumental magnitudes (using counts)
+counts = phot_table['residual_aperture_sum']
+mag_inst = -2.5 * np.log10(counts)
+phot_table['mag_inst'] = mag_inst
+
 print(phot_table)
-print(type(phot_table))
 
 # write phot_table to a text file
 file = open(path + fname + '_sources.txt', 'w')
-phot_table.write(file, format='ascii.fixed_width')
+phot_table.write(file, format='csv')
 file.close()
 
+'''
 # plot the image and mark the location of detected sources
 norm = ImageNormalize(img, interval=ZScaleInterval())
 plt.figure(figsize=(9,12))          # set figure size
 plt.imshow(img, cmap='Greys', origin='lower', norm=norm)
 apertures.plot(color='green', lw=1., alpha=0.5)
 annulus_apertures.plot(color='green', lw=1., alpha=0.5)
-plt.savefig(path + fname + '_apphot.pdf', format='pdf', bbox_inches='tight', dpi=600)
+#plt.savefig(path + fname + '_apphot.pdf', format='pdf', bbox_inches='tight', dpi=600)
 plt.show()
+'''
